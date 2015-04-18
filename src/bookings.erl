0482200aca3d9e56a9392006567972054f7b364b
Ulf Angermann <ua@innoq.com>
2015-04-18 17:46:52 +0700

    added function for the year only

--- src/bookings.erl
+++ src/bookings.erl
@@ -19,10 +19,13 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -export([start_link/0]).
 -export([start/0]).
--export([get_eater/2]).
+-export([get_eater/1, get_eater/2]).
 %% ====================================================================
 %% External functions
 %% ====================================================================
+get_eater(Year) ->
+    gen_server:call({global, ?MODULE}, {get_eater, Year}).
+
 get_eater(Year, Month) ->
     gen_server:call({global, ?MODULE}, {get_eater, Year, Month}).
 %% --------------------------------------------------------------------
@@ -62,6 +65,11 @@ init([]) ->
 %%          {stop, Reason, Reply, State}   | (terminate/2 is called)
 %%          {stop, Reason, State}            (terminate/2 is called)
 %% --------------------------------------------------------------------
+handle_call({get_eater, Year}, From, State) ->
+    Menus = get_menus_for_year(Year),
+    {Intern, Extern} = analyse_menus(year, Menus, array:new(12, {default,0}), array:new(12, {default,0})),
+    {reply, {Year, [{intern, Intern}, {extern, Extern}]}, State};
+
 handle_call({get_eater, Year, Month}, From, State) ->
     Menus = get_menus_for_month(Year, Month),
     {Intern, Extern} = analyse_menus(month, Menus, 0, 0),
@@ -110,6 +118,12 @@ code_change(OldVsn, State, Extra) ->
 %% --------------------------------------------------------------------
 %%% Internal functions
 %% --------------------------------------------------------------------
+get_menus_for_year(Year) when is_list(Year) ->
+    get_menus_for_year(list_to_integer(Year));
+get_menus_for_year(Year) ->
+    boss_db:find(menu, [{date, 'ge', {{Year, 1,1}, {0,0,0}}}, {date, 'le', {{Year, 12, 31}, {0,0,0}}}]).
+
+
 get_menus_for_month(Year, Month) when is_list(Year) ->  
     get_menus_for_month(list_to_integer(Year), list_to_integer(Month));
 get_menus_for_month(Year, Month) -> 
@@ -119,7 +133,14 @@ analyse_menus(month,[], Intern, Extern) ->
     {Intern, Extern};   
 analyse_menus(month, [H|T], Intern, Extern) ->
     {Int_count, Ext_count} = count_bookings(H:booking()),
-    analyse_menus(month,T, Int_count + Intern, Ext_count + Extern).
+    analyse_menus(month,T, Int_count + Intern, Ext_count + Extern);
+
+analyse_menus(year,[], Intern, Extern) ->
+    {list_of_integer_to_string(array:to_list(Intern)), list_of_integer_to_string(array:to_list(Extern))};   
+analyse_menus(year, [H|T], Intern, Extern) ->
+    {{_Y, Month, _D}, _Time} = H:date(),
+    {Int_count, Ext_count} = count_bookings(H:booking()),
+    analyse_menus(year,T, add(Month, Int_count, Intern), add(Month, Ext_count, Extern)).
 
 count_bookings(Bookings) ->
     count_bookings(Bookings, 0, 0). 
@@ -131,6 +152,15 @@ count_bookings([H|T], Intern, Extern) ->
         true -> count_bookings(T, Intern + 1, Extern);
         false ->count_bookings(T, Intern, Extern + 1)
     end.    
+
+add(Month, Value, Array) ->
+    Index = Month - 1,
+    Act_value = array:get(Index, Array),
+    array:set(Index, Act_value + Value, Array).
+
+list_of_integer_to_string(List_of_integer) ->
+    string:join([integer_to_list(S) || S <- List_of_integer],",").
+
 %% --------------------------------------------------------------------
 %%% Test functions
 %% --------------------------------------------------------------------
